7c6cd707e9c904fa476cb6437a163038e06829c3
Paulo Sergio <pauloserg861@gmail.com>
2023-01-30 11:37:21 -0300

    criando revelador de senha

---
+++ src/components/atoms/PasswordEye/PasswordRevelling.vue
@@ -0,0 +1,28 @@
+<template>
+	<span>
+		<Icon icon="carbon:view" id="view-icon" v-if="isPassword"	/>
+		<Icon icon="carbon:view-off" id="view-icon" v-else/>
+	</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Icon } from "@iconify/vue";
+
+export default defineComponent({
+	name: "PassewordRevelling",
+	components: { Icon },
+	emits: ["toggle"],
+	props: {
+		isPassword: {
+			type: Boolean
+		}
+	}
+});
+</script>
+
+<style lang="sass" scoped>
+svg
+	display: flex
+	cursor: pointer
+</style>
