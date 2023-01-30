13e668e60c57ceacf22bed31829672d7ea1feab5
Paulo Sergio <pauloserg861@gmail.com>
2023-01-30 11:48:17 -0300

    implementando estrutura atomica

---
+++ src/components/molecules/LoginInputs/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<form>
+		<BaseInput />
+	</form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BaseInput from "@/components/atoms/Inputs/BaseInput.vue";
+
+export default defineComponent({
+	name: "LoginInputs",
+	components: { BaseInput }
+});
+</script>
---
+++ src/components/organisms/Forms/FormLogin.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="box">
+		<div class="box_logo">
+			<img :src="logo" alt="logo principal">
+		</div>
+		<p>Você está no sistema de gestão da plataforma / e-commerce DevTech</p>
+		<LoginInputs />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LoginInputs from '@/components/molecules/LoginInputs/index.vue'
+
+import logo from '@/assets/img/logo-dev-tech-nobg.png';
+
+export default defineComponent({
+	name: "FormLogin",
+	components: { LoginInputs },
+	data() {
+		return {
+			logo: logo,
+		}
+	}
+});
+</script>
+
+<style lang="sass" scoped>
+.box
+	background-color: #fff
+	width: 40%
+	padding: 30px
+	border-radius: 16px
+	box-shadow: -2px 2px 10px rgba(0, 0, 0, 0.166)
+
+	p
+		text-align: center
+		padding: 10px 80px
+
+	&_logo
+		text-align: center
+
+		img
+			width: 30%
+</style>
