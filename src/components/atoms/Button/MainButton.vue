737f4afe564be90b1a63a310206eb05f2c830c4f
Paulo Sergio <pauloserg861@gmail.com>
2023-01-30 13:17:16 -0300

    criando componente de botao

---
+++ src/components/atoms/Button/MainButton.vue
@@ -0,0 +1,27 @@
+<template>
+	<button class="base-style">
+		{{ btn_title }}
+	</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "MainButton",
+	props: {
+		btn_title: {
+			type: String,
+		}
+	},
+});
+</script>
+
+<style lang="sass" scoped>
+.base-style
+	height: 35px
+	padding: 0 25px
+	border: 1px solid gray
+	border-radius: 8px
+	cursor: pointer
+</style>
