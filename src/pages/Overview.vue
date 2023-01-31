79e54aab56a0e7a5da476828437e2e75be8f6779
Paulo Sergio <pauloserg861@gmail.com>
2023-01-30 21:26:27 -0300

    criando estrutura de template

---
+++ src/pages/Overview.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		overview
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: 'Overview'
+});
+</script>
+
+<style lang="scss" scoped>
+div {
+	background: red;
+}
+</style>
---
+++ src/pages/layout/DashboardLayout.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="wrapper">
+		<Sidebar />
+		<div class="main-painel">
+			<p>Header</p>
+			<main>
+				<Content />
+			</main>
+			<p>footer</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Content from "./Content.vue";
+import Sidebar from '@/components/organisms/Sidebar/index.vue';
+
+export default defineComponent({
+  components: { Content, Sidebar },
+	name: 'DashboardLayout',
+});
+</script>
+
+<style scoped lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+	transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+	opacity: 0;
+}
+.wrapper {
+	display: flex;
+	max-width: 100%;
+	margin: 0 auto;
+	margin-bottom: 20px;
+	height: 100%;
+
+	.main-painel {
+		width: 85%;
+		min-height: 100vh;
+		background: red;
+
+		main {
+			height: 84%;
+		}
+	}
+}
+</style>
