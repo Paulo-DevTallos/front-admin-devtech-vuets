fb565c7c1c8378478e5bca29e6b825cec351e28f
Paulo Sergio <pauloserg861@gmail.com>
2023-01-31 13:41:57 -0300

    criando arquivo de rotas para o menu

---
+++ constansts/router-links.ts
@@ -0,0 +1,38 @@
+export default [
+	{
+		id: 0,
+		router: "/app/dashboard",
+		icon: "chart-combo-stacked",
+		description: "Dashboard",
+	},
+	{
+		id: 1,
+		router: "/app/customers",
+		icon: "user-multiple",
+		description: "Usuários",
+	},
+	{
+		id: 2,
+		router: "/app/books",
+		icon: "book",
+		description: "Livros",
+	},
+	{
+		id: 3,
+		router: "/app/comments",
+		icon: "add-comment",
+		description: "Comentários",
+	},
+	{
+		id: 4,
+		router: "/app/authors",
+		icon: "license-third-party",
+		description: "Autores",
+	},
+	{
+		id: 5,
+		router: "/app/sessions",
+		icon: "report",
+		description: "Sessões",
+	},
+];
