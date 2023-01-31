2a250927db2624029a3247ead3cc6c4a0aeb7b9a
Paulo Sergio <pauloserg861@gmail.com>
2023-01-31 13:54:04 -0300

    criando pagina de Customer

---
+++ src/pages/Customers.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		customers
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'Customers'
+});
+</script>
