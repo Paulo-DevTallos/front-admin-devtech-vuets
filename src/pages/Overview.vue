421fbca4b8d5b40f3041dac5e160eb204fa87d43
Paulo Sergio <pauloserg861@gmail.com>
2023-01-31 13:52:16 -0300

    implementando MenagerCard na Overview

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -1,19 +1,39 @@
 <template>
-	<div>
-		overview
+	<div class="cards-data">
+		<MenagerCard
+			:card_message="'Usuários ativos'"
+			:count_value="'300'"
+		/>
+		<MenagerCard
+			:card_message="'uma mensagem'"
+			:count_value="'300'"
+		/>
+		<MenagerCard
+			:card_message="'uma mensagem'"
+			:count_value="'300'"
+		/>
+		<MenagerCard
+			:card_message="'uma mensagem'"
+			:count_value="'300'"
+		/>
 	</div>
 </template>
 
 <script lang="ts">
+import MenagerCard from "@/components/atoms/Cards/MenagerCard.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-	name: 'Overview'
+	name: "Overview",
+	components: { MenagerCard }
 });
 </script>
 
 <style lang="scss" scoped>
-div {
-	background: red;
+.cards-data {
+	display: flex;
+	justify-content: space-around;
+	width: 100%;
+	padding: 0 20px;
 }
 </style>
